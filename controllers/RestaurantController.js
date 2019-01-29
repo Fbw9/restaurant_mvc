7381780f3d8fba25f80be993a5bb066aa43fc7b7
Ryan Kretch <rkretch91@gmail.com>
2019-01-29 15:13:39 +0100

    done

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -4,6 +4,17 @@ const Restaurant = mongoose.model('Restaurant', restaurantSchema)
 
 const restaurantController = {}
 
+//LIST ALL
+restaurantController.list = (req, res) => {
+  Restaurant.find({}).exec((error, restaurants) => {
+    if (error) {
+      console.log('Error:', error)
+    } else {
+      res.render('../views/restaurants/index', {restaurants: restaurants})
+    }
+  })
+}
+
 //CREATE METHOD
 restaurantController.create = (req, res) => {
   res.render('../views/restaurants/create')
@@ -22,11 +33,62 @@ restaurantController.save = (req, res) => {
       res.render('restaurants/create')
     } else {
       console.log('Restaurant was created successfully')
-      res.redirect('/')
+      res.redirect(`/restaurants/show/${restaurant._id}`)
     }
   })
-
 }
+
+//SHOW METHOD
+restaurantController.show = (req, res) => {
+  Restaurant.findOne({_id: req.params.id}).exec((error, restaurant) => {
+    if(error) {
+      console.log('Error:', error)
+    } else {
+      res.render('../views/restaurants/show', {restaurant: restaurant})
+    }
+  })
+}
+
+//EDIT
+restaurantController.edit = (req, res) => {
+  Restaurant.findOne({_id: req.params.id}).exec((error, restaurant) => {
+    if(error) {
+      console.log(error)
+    } else {
+      res.render('../views/restaurants/edit', {restaurant: restaurant})
+    }
+  })
+}
+
+
+//UPDATE
+restaurantController.update = (req, res) => {
+  Restaurant.findByIdAndUpdate(req.params.id, { $set: {
+    name: req.body.name,
+    description: req.body.description,
+    rating: req.body.rating,
+    open: req.body.open
+  }}, { new: true }, (error, restaurant) => {
+    if(error) {
+      console.log(error)
+      res.render('../views/restaurants/edit', {restaurant: req.body})
+    } else {
+      res.redirect(`/restaurants/show/${restaurant._id}`)
+    }
+  })
+}
+  //DELETE
+  restaurantController.delete = (req, res) => {
+    Restaurant.remove({_id: req.params.id}, (error) => {
+      if(error) {
+        console.log(error)
+      } else {
+        console.log('Restaurant has been deleted!!! ')
+        res.redirect('/restaurants')
+      }
+    })
+  }
+
 
 
 
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,8 +3,19 @@ const router = express.Router()
 
 const restaurant = require('../controllers/RestaurantController.js')
 
+//Get all restaurants
+router.get('/', restaurant.list)
+//Create restaurant
 router.get('/create', restaurant.create)
-
+//Save restaurant
 router.post('/save', restaurant.save)
+//Get single restaurant by id
+router.get('/show/:id', restaurant.show)
+//Edit restaurant
+router.get('/edit/:id', restaurant.edit)
+//Update restaurant
+router.post('/update/:id', restaurant.update)
+//Delete restaurant
+router.post('/delete/:id', restaurant.delete)
 
 module.exports = router
